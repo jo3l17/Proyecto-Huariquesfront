d93360e52b0b2fc24ef0e86e0ea08393d714ace1
jo3l17 <joelvaldezangeles@gmail.com>
2019-05-29 10:20:52 -0500

    router final final

--- huariques/src/app/app-routing.module.ts
+++ huariques/src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
