70432073612310614833f1ac904fa05933c89519
jo3l17 <joelvaldezangeles@gmail.com>
2019-05-29 10:13:28 -0500

    routing creado

--- huariques/src/app/app-routing/app-routing.module.ts
+++ huariques/src/app/app-routing/app-routing.module.ts
@@ -1,14 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent
-  }
-]
 @NgModule({
   declarations: [],
   imports: [
