bca6c41d4bdb48f70d95b0f7678c875d199a7094
jo3l17 <joelvaldezangeles@gmail.com>
2019-05-30 16:50:00 -0500

    avance proyecto

--- huariquesfront/src/app/components/rest-details/rest-details.component.ts
+++ huariquesfront/src/app/components/rest-details/rest-details.component.ts
@@ -3,14 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rest-details',
   templateUrl: './rest-details.component.html',
-  styleUrls: ['./rest-details.component.scss']
+  styleUrls: [
+    './rest-details.component.scss',
+    '../../../assets/royalslider/royalslider.css',
+    '../../../assets/preview-assets/css/resete166.css',
+    '../../../assets/royalslider/skins/universal/rs-universale166.css'
+  ]
 })
 export class RestDetailsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
